3d365ac3956b21395d285b097e9ef95677ffd51e
quentingourier <73183451+quentingourier@users.noreply.github.com>
2022-03-28 10:41:04 +0200

    Update traffic_state.vhd

--- traffic_state.vhd
+++ traffic_state.vhd
@@ -12,55 +12,53 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity traffic_state is
     Port ( clk : in  STD_LOGIC;
-			  rst : in std_logic;
+            rst : in std_logic;
            a : out  STD_LOGIC_VECTOR (2 downto 0);
            b : out  STD_LOGIC_VECTOR (2 downto 0));
 end traffic_state;
 
 architecture Behavioral of traffic_state is
 
-	
-
-	type Statetype is(E0,E1,E2,E3,E4,E5);
-
-	signal etat : Statetype;
-	signal count 	:integer:=1;
 
 
-	
+    type Statetype is(E0,E1,E2,E3,E4,E5);
+
+    signal etat_reg,etat_next : Statetype;
+    signal count_reg,count_next     :integer:=0;
+[09:40]
 begin
-	process(clk)
-		begin
-			if clk' event and clk='1' then
-				if rst = '1' then
-					etat <= E0;
-					count <= 0;
-				end if;
-				
-				case etat is
-				
-					when E0 => a <="100"; b<="001";count <= count+1;
-								if count>4 and count<6 then etat<=E1;
-								else etat<=E0; end if;
-					when E1 => a <="010"; b<="001";count <= count+1;
-								if count>5 and count<7 then etat<=E2;
-								else etat<=E1; end if;
-					when E2 => a <="001"; b<="001";count <= count+1;
-								if count>6 and count<8 then etat<=E3;
-								else etat<=E2; end if;
-					when E3 => a <="001"; b<="100";count <= count+1;
-								if count>11 and count<13 then etat<=E4;
-								else etat<=E3; end if;
-					when E4 => a <="001"; b<="010";count <= count+1;
-								if count>12 and count<14 then etat<=E5;
-								else etat<=E4; end if;
-					when E5 => a <="001"; b<="001";count <= count+1;
-								if count>13 then etat<=E0;count <=0 ;
-								else etat<=E5; end if; 
-				end case;
-			end if;
-	end process;
-		
-
+    process(etat_reg,count_reg)
+        begin
+              case etat_reg is
+                    when E0 => a <="100"; b<="001";count_next <= count_reg+1;
+                                if count_reg>4 and count_reg<6 then etat_next<=E1;
+                                else etat_next<=E0; end if;
+                    when E1 => a <="010"; b<="001";count_next <= count_reg+1;
+                                if count_reg>5 and count_reg<7 then etat_next<=E2;
+                                else etat_next<=E1; end if;
+                    when E2 => a <="001"; b<="001";count_next <= count_reg+1;
+                                if count_reg>6 and count_reg<8 then etat_next<=E3;
+                                else etat_next<=E2; end if;
+                    when E3 => a <="001"; b<="100";count_next<= count_reg+1;
+                                if count_reg>11 and count_reg<13 then etat_next<=E4;
+                                else etat_next<=E3; end if;
+                    when E4 => a <="001"; b<="010";count_next <= count_reg+1;
+                                if count_reg>12 and count_reg<14 then etat_next<=E5;
+                                else etat_next<=E4; end if;
+                    when E5 => a <="001"; b<="001";count_next <= count_reg+1;
+                                if count_reg>13 then etat_next<=E0;count_next <=0 ;
+                                else etat_next<=E5; end if; 
+              end case;
+    end process;
+process (clk,rst)
+begin
+        if rst='1' then
+            etat_reg <=E0;
+            count_reg<=0;
+        else if clk'event and clk = '1' then
+            etat_reg <=etat_next;
+            count_reg <=count_next;
+        end if;
+    end if;
+end process;
 end Behavioral;
-
